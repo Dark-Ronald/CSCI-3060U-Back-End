3754346877f2f2a92e47485773f3e9a66450e7af
James LeBlanc <james.leblanc1@ontariotechu.net>
2020-03-09 13:21:37 -0400

    added item and fileIO functions

--- BackEnd/src/FileIO.java
+++ BackEnd/src/FileIO.java
@@ -1,15 +1,46 @@
 import java.util.ArrayList;
-import user.java;
-import Item.java;
+import java.io.*;
 
 public class FileIO{
 
+    /*private String userFilePath;
+    private String itemFilePath;
+    private String transactionFilePath;
+
     public FileIO (String userFilePath, String itemFilePath, String transacitonFilePath){
 
+    }*/
+
+    public static boolean dailyProcess(ArrayList<user> users, ArrayList<Item> items, ArrayList<String> transactions){
+
+        return true;
     }
 
-    public static void dailyProcess(ArrayList<User> users, ArrayList<Item> items, ArrayList<String> transactions){
+    public static boolean readUsersFile(ArrayList<user> users){
 
+        return true;
+    }
+
+    public static boolean readItemsFile(ArrayList<Item> items){
+
+        return true;
+    }
+
+    public static boolean readTransactionFile(ArrayList<String> transacitons){
+
+
+        return true;
+    }
+
+
+    public static boolean updateUsersFile(ArrayList<user> users){
+
+        return true;
+    }
+
+    public static boolean updateItemsFile(ArrayList<Item> items){
+        
+        return true;
     }
 
 }
--- BackEnd/src/Item.java
+++ BackEnd/src/Item.java
@@ -1,6 +1,91 @@
 public class Item {
 
-    public Item(){
-        
+    private String itemName;
+    private String sellerName;
+    private String buyerName;
+    private double bidPrice;
+    private short remaningDays; 
+
+    public Item(String itemName, String sellerName, String buyerName, double bidPrice, short remainingDays){
+        this.itemName = itemName;
+        this.sellerName = sellerName;
+        this.buyerName = buyerName;
+        this.bidPrice = bidPrice;
+        this.remaningDays = remainingDays;
+    }
+
+    /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * @return the sellerName
+     */
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    /**
+     * @return the buyerName
+     */
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    /**
+     * @return the bidPrice
+     */
+    public double getBidPrice() {
+        return bidPrice;
+    }
+
+    /**
+     * @return the remaningDays
+     */
+    public short getRemaningDays() {
+        return remaningDays;
+    }
+
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @param sellerName the sellerName to set
+     */
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    /**
+     * @param buyerName the buyerName to set
+     */
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    /**
+     * @param bidPrice the bidPrice to set
+     */
+    public void setBidPrice(double bidPrice) {
+        this.bidPrice = bidPrice;
+    }
+
+    /**
+     * 
+     */
+    public void decrementRemaningDays() {
+        this.remaningDays--;
+    }
+
+    @Override
+    public String toString() {
+        return "item description";
     }
 }
