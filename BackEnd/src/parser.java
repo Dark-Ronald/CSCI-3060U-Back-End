311fca9624d1b1a4568b950c080e9d23978f3e4c
Dark-Ronald <philip.walton@ontariotechu.net>
2020-03-16 01:01:04 -0700

    added comments

--- BackEnd/src/parser.java
+++ BackEnd/src/parser.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 import java.util.Date;
 
+/*
+utility class for parsing the lines of the daily_transaction file
+ */
 public class parser {
     static ArrayList<user> currentUserAccounts = new ArrayList<user>();
     static ArrayList<Item> availableItems = new ArrayList<Item>();
     static Date datePreviouslyRun;
 
+    /*
+    gets the user to add credit to, then sets their credit to the credit value in the
+    transaction
+    input: transaction: a line of the daily_transaction file with addcredit code
+    output: None
+     */
     static void addCredit(String transaction) {
         String username = transaction.substring(3, 18);
         double credit = Double.valueOf(transaction.substring(23, 31));
@@ -16,20 +25,55 @@ public class parser {
             }
         }
     }
-    static void advertise() {
+
+    /*
+    adds an item to the availableItems list
+    input: transaction: a line of the daily_transaction file with advertise code
+    output: None
+     */
+    static void advertise(String transaction) {
 
     }
-    static void bid() {
+
+    /*
+    handles bid transaction
+    input: transaction: a line of the daily_transaction file with bid code
+    output: None
+     */
+    static void bid(String transaction) {
 
     }
-    static void deleteUser() {
+
+    /*
+    adds user to currentUserAccounts list
+    input: transaction: a line of the daily_transaction file with create code
+    output: None
+     */
+    static void create(String transaction) {
+
+    }
+
+    /*
+    removes user from currentUserAcoounts list, removes outstanding items user has up for
+    auction (if any), and removes them from any item bids (if any) and replaces them with next
+    highest bidder (if any)
+    input: transaction: a line of the daily_transaction file with delete code
+    output: None
+     */
+    static void deleteUser(String transaction) {
         /*
         if user being deleted is the highest bidder on an item, then when they are deleted
         the second highest bidder needs to be found and replace the deleted user in the item
         object.  This may involve reading previous dailyTransaction files
         */
     }
-    static void refund() {
+
+    /*
+    refunds user
+    input: transaction: a line of the daily_transaction file with create code
+    output: None
+     */
+    static void refund(String transaction) {
 
     }
 }
